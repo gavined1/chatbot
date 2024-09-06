d2d555f752511c43da67cfaafc55380c746f18bc
Gavin <tangsoklim7@gmail.com>
2024-09-06 21:45:09 +0700

    Update data.js

--- functions/data.js
+++ functions/data.js
@@ -1,6 +1,6 @@
 // netlify-functions/data.js
 module.exports = [{
-        "input": ["wassup gavin", "what's up gavin", "wassup gvin", "what's up gvin", "wassup g'vin", "what's up g'vin", "hello gavin", "helloo gavin", "hellooo gavin", "helo gavin", "hi gavin", "hii gavin", "hiii gavin", "hello gvin", "helloo gvin", "hellooo gvin", "helo gvin", "hi gvin", "hii gvin", "hiii gvin", "hello g'vin", "helloo g'vin", "hellooo g'vin", "helo g'vin", "hi g'vin", "hii g'vin", "hiii g'vin"],
+        "input": ["wassup gavin", "what's up gavin", "wassup gvin", "what's up gvin", "wassup g'vin", "what's up g'vin", "hello gavin", "helloo gavin", "hellooo gavin", "helo gavin", "hi gavin", "hii gavin", "hiii gavin", "hello gvin", "helloo gvin", "hellooo gvin", "helo gvin", "hi gvin", "hii gvin", "hiii gvin", "hello g'vin", "helloo g'vin", "hellooo g'vin", "helo g'vin", "hi g'vin", "hii g'vin", "hiii g'vin", "hi"],
         "response": [
             "Gavin is currently unavailable, but I am his assistant. How can I assist you today?",
             "Gavin is a bit busy at the moment, but I’m here to help. What can I do for you?",

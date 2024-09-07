6d11954c16e2fbaa93bee12583eedb010c4685b7
Gavin <105969495+gavined1@users.noreply.github.com>
2024-09-07 21:23:51 +0700

    Delete functions directory

--- functions/data.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-    "input": ["wassup gavin", "what's up gavin", "wassup gvin", "what's up gvin", "wassup g'vin", "what's up g'vin", "hello gavin", "helloo gavin", "hellooo gavin", "helo gavin", "hi gavin", "hii gavin", "hiii gavin", "hello gvin", "helloo gvin", "hellooo gvin", "helo gvin", "hi gvin", "hii gvin", "hiii gvin", "hello g'vin", "helloo g'vin", "hellooo g'vin", "helo g'vin", "hi g'vin", "hii g'vin", "hiii g'vin", "hi"],
-    "response": [
-        "Gavin is currently unavailable, but I am his assistant. How can I assist you today?",
-        "Gavin is a bit busy at the moment, but I’m here to help. What can I do for you?",
-        "Gavin is not available right now, but I can assist you in his place. How can I help?",
-        "Gavin is occupied at the moment, but I’m here to assist. How may I be of service?"
-    ]
-}];
